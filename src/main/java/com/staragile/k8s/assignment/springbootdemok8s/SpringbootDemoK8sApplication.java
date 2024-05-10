777549a900a15c6dd24dd82988190986dfd0b744
Akshay <akshay@amit.com>
2024-05-10 19:38:16 +0530

    cm1

---
+++ src/main/java/com/staragile/k8s/assignment/springbootdemok8s/SpringbootDemoK8sApplication.java
@@ -0,0 +1,13 @@
+package com.staragile.k8s.assignment.springbootdemok8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoK8sApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoK8sApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/staragile/k8s/assignment/springbootdemok8s/SpringbootDemoK8sApplicationTests.java
@@ -0,0 +1,13 @@
+package com.staragile.k8s.assignment.springbootdemok8s;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoK8sApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
